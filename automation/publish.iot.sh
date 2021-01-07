eac6e67303188281e0a943f6db9cb679e0b6c344
phytofy <phytofysupport@osram.com>
2021-01-07 17:27:14 +0100

    Unified naming & version tagging

--- automation/publish.iot.sh
+++ automation/publish.iot.sh
@@ -6,22 +6,20 @@ SELF=$0
 REAL=$(realpath "$SELF")
 BASE=$(dirname "$REAL")
 
-VERSION=$(git describe --tags --dirty)
-
 cd $BASE/..
 docker login -u ${REGISTRY_USER} -p ${REGISTRY_TOKEN}
-docker tag phytofy-amd64:latest ${REGISTRY_PREFIX}amd64${VERSION_SEPARATOR}${VERSION}
+docker tag phytofy-amd64:latest ${REGISTRY_PREFIX}amd64${VERSION_SEPARATOR}${RELEASE_VERSION}
 docker tag phytofy-amd64:latest ${REGISTRY_PREFIX}amd64${VERSION_SEPARATOR}latest
-docker tag phytofy-arm32v7:latest ${REGISTRY_PREFIX}arm32v7${VERSION_SEPARATOR}${VERSION}
+docker tag phytofy-arm32v7:latest ${REGISTRY_PREFIX}arm32v7${VERSION_SEPARATOR}${RELEASE_VERSION}
 docker tag phytofy-arm32v7:latest ${REGISTRY_PREFIX}arm32v7${VERSION_SEPARATOR}latest
-docker push ${REGISTRY_PREFIX}amd64${VERSION_SEPARATOR}${VERSION}
+docker push ${REGISTRY_PREFIX}amd64${VERSION_SEPARATOR}${RELEASE_VERSION}
 docker push ${REGISTRY_PREFIX}amd64${VERSION_SEPARATOR}latest
-docker push ${REGISTRY_PREFIX}arm32v7${VERSION_SEPARATOR}${VERSION}
+docker push ${REGISTRY_PREFIX}arm32v7${VERSION_SEPARATOR}${RELEASE_VERSION}
 docker push ${REGISTRY_PREFIX}arm32v7${VERSION_SEPARATOR}latest
 docker rmi phytofy-amd64:latest
-docker rmi ${REGISTRY_PREFIX}amd64${VERSION_SEPARATOR}${VERSION}
+docker rmi ${REGISTRY_PREFIX}amd64${VERSION_SEPARATOR}${RELEASE_VERSION}
 docker rmi ${REGISTRY_PREFIX}amd64${VERSION_SEPARATOR}latest
 docker rmi phytofy-arm32v7:latest
-docker rmi ${REGISTRY_PREFIX}arm32v7${VERSION_SEPARATOR}${VERSION}
+docker rmi ${REGISTRY_PREFIX}arm32v7${VERSION_SEPARATOR}${RELEASE_VERSION}
 docker rmi ${REGISTRY_PREFIX}arm32v7${VERSION_SEPARATOR}latest
 docker logout
