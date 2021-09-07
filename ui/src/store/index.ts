fe3e0dc3ca8b246473ee1da9e16b97aaabd1f563
omgrnd <omgrnd@gmail.com>
2021-09-07 08:42:26 +0200

    Added client-side scheduling persistence

--- ui/src/store/index.ts
+++ ui/src/store/index.ts
@@ -2,8 +2,31 @@
 
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
 
 Vue.use(Vuex);
+
+export interface Schedule {
+  id: string;
+  selected: boolean;
+  startDate: string;
+  stopDate: string;
+  startTime: string;
+  stopTime: string;
+  levels: number[];
+  serial: number | null;
+}
+
+interface RootState {
+  eulaVisible: boolean;
+  eulaAccepted: boolean;
+  serials: number[];
+  schedules: Schedule[];
+}
+
+const persistence = new VuexPersistence<RootState>({
+  storage: window.localStorage
+});
 
 const eulaAcceptedInitialState = window.localStorage.getItem("eulaAccepted") === null ? false : window.localStorage.getItem("eulaAccepted") === "true";
 
@@ -12,6 +35,7 @@ export default new Vuex.Store({
     eulaVisible: !eulaAcceptedInitialState,
     eulaAccepted: eulaAcceptedInitialState,
     serials: [] as number[],
+    schedules: [] as Schedule[],
   },
   mutations: {
     setEulaVisible(state, eulaVisible: boolean) {
@@ -23,8 +47,12 @@ export default new Vuex.Store({
     },
     setSerials(state, serials: number[]) {
       state.serials = serials;
+    },
+    setSchedules(state, schedules: Schedule[]) {
+      state.schedules = schedules;
     }
   },
   actions: {},
-  modules: {}
+  modules: {},
+  plugins: [persistence.plugin]
 });
